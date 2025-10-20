fafe94092569a282636903b262385f02f7493c39
Jc Canlas <Johnrolandchristopher@gmail.com>
2025-10-20 11:31:22 +0800

    Create init_database.sql for DataWarehouse setup
    This script creates a new database named 'DataWarehouse' and sets up three schemas: 'bronze', 'silver', and 'gold'. It also includes a warning about dropping the existing database if it exists.

---
+++ scripts/init_database.sql
@@ -0,0 +1,43 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+-- Master is a default database in SQL Server where you can create other databases.
+-- GO command separate batches when working with multiple SQL statements.
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schema (bronze, silver, gold)
+
+CREATE SCHEMA bronze;
+GO 
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
